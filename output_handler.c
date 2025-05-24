390da366e81e9ff2164082e005ef983e6adbc56a
tboltondev <tbolton96@icloud.com>
2025-05-24 21:08:45 +0100

    format

--- output_handler.c
+++ output_handler.c
@@ -1,6 +1,4 @@
 #include "output_handler.h"
 #include "search_result.h"
 
-void to_stdout(SearchResult sr) {
-  print_search_result(sr);
-}
+void to_stdout(SearchResult sr) { print_search_result(sr); }
