45485dcf63f5099cc42710bbd28dee6d2db79b46
tboltondev <tbolton96@icloud.com>
2025-06-16 00:33:39 +0100

    remove unneeded signatures in header files

--- include/searcher.h
+++ include/searcher.h
@@ -6,16 +6,6 @@
 
 typedef void (*ResultHandler)(SearchResult);
 
-SearchStatus search_file(SearchResult *sr, const char *pattern, const char *path);
-
-SearchStatus handle_search_file(const char *pattern, const char *path,
-                        ResultHandler result_handler);
-
-SearchStatus search_dir_recursively(
-    const char *pattern, const char *base_path, ResultHandler result_handler,
-    int current_depth,
-    int max_depth); // TODO: exclude .gitignore files / dirs, binaries etc
-
 SearchStatus search(const char *pattern, const char *path,
             ResultHandler result_handler);
 
